2d4ba517e92f83765f2bd2b642f7906fbb6c21bb
Adejar Junior <adejarjunior18@gmail.com>
2021-06-21 22:23:23 -0300

    View da situação atual

---
+++ client/src/views/current-situation/index.js
@@ -0,0 +1,94 @@
+import React, { useState } from 'react'
+import Header from '../../components/Header';
+import { InputGroup, InputGroupAddon, Input } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
+import { Button } from 'reactstrap';
+import { Table } from 'reactstrap';
+import Alert from '../../components/Alert';
+
+function Situation() {
+
+    let [stockInput, setInput] = useState('');
+    let [response, setResponde] = useState();
+
+    const [visibleAlert, setVisibleAlert] = useState(false);
+    const [messageAlert, setMessageAlert] = useState('');
+
+    function handleinputChange(event) {
+        setInput(event.target.value);
+    }
+
+    const loadData = async () => {
+        console.log(stockInput)
+        const url = `http://localhost:3030/stocks/${stockInput}/quote`;
+        const response = await fetch(url);
+        const data = await response.json();
+        if (response.status === 200) {
+            setResponde(data)
+        } else {
+            setVisibleAlert(true)
+
+            setTimeout(function () {
+                setVisibleAlert(false)
+            }, 8000);
+
+            setMessageAlert(data.error)
+        }
+    }
+
+    return (
+        <div>
+            <Header />
+            <div>
+                <Container>
+                    <Row>
+                        <Alert visibleAlert={visibleAlert} messageAlert={messageAlert} />
+                        <Col sm="12" md={{ size: 6, offset: 3 }}>
+                            <h3>Informe o nome da ação:</h3>
+                            <InputGroup>
+                                <Input onChange={handleinputChange} />
+                                <InputGroupAddon addonType="append">
+                                </InputGroupAddon>
+                            </InputGroup>
+                        </Col>
+                    </Row><br /><br /><br />
+                    <Row>
+                        <Col xs="6" sm="4"></Col>
+                        <Col xs="6" sm="4"></Col>
+                        <Col sm="4">
+                            <Button color="primary" onClick={loadData}>BUSCAR</Button>
+                        </Col>
+
+                    </Row><br /><br /><br />
+                    <Col>
+                        <Table>
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Ação</th>
+                                    <th>Ultimo Preço</th>
+                                    <th>Data</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {response &&
+                                    <tr>
+                                        <td>1</td>
+                                        <td>{response.name}</td>
+                                        <td>{response.lastPrice}</td>
+                                        <td>{response.pricedAt}</td>
+                                    </tr>
+                                }
+
+                            </tbody>
+                        </Table>
+                    </Col>
+                </Container>
+                <div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Situation;
