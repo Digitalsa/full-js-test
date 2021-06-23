842ec4daa45b5edfd1753c1bdb8cd8bc271a7096
Adejar Junior <adejarjunior18@gmail.com>
2021-06-22 23:12:54 -0300

    Alterou a cor da navbar para azul e link colocados em negrito

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -14,20 +14,20 @@ const Header = (props) => {
 
   return (
     <div>
-      <Navbar color="light" light expand="md">
+      <Navbar style={{ backgroundColor: '#0d6efd' }} color="faded" dark expand="md">
         <Collapse isOpen={isOpen} navbar>
           <Nav className="mr-auto" navbar>
             <NavItem>
-              <NavLink href="/cotacao_recente">Cotação Atual</NavLink>
+              <NavLink href="/cotacao_recente"><b>Cotação Atual</b></NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href="/historico_preco">Historico de preços</NavLink>
+              <NavLink href="/historico_preco"><b>Historico de preços</b></NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href="/comparar_preco">Comparar preços</NavLink>
+              <NavLink href="/comparar_preco"><b>Comparar preços</b></NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href="/projecao_ganhos">Projeção de ganhos</NavLink>
+              <NavLink href="/projecao_ganhos"><b>Projeção de ganhos</b></NavLink>
             </NavItem>
           </Nav>
         </Collapse>
